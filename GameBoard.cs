b31470f627e8b7abf4fd5ce201cb9027e567e6b5
WANGYAWE <93184071+WANGYAWE@users.noreply.github.com>
2022-01-18 12:14:53 +0800

    Add files via upload

--- GameBoard.cs
+++ GameBoard.cs
@@ -1,88 +1,190 @@
-using System;
-namespace Xiangqi{
-    class GameBoard 
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Collections;
+
+namespace XiangqiGame
+{
+    class GameBoard
     {
-        string Name;
-        int columns;
-        int rows;
-        public chesspiece[,] board;
-        public GameBoard(string name,int columns,int rows){
-            this.Name = name;
-            this.columns = columns;
-            this.rows = rows;
-            this.board = new chesspiece[columns,rows];
+        //用来储存每个位置的数据,在grid上
+        public chesspiece[,] board = new chesspiece[10,9];
+       
+        //GameBoard board1 = new GameBoard();
+        int orginX = -1;
+        int orginY = -1;
+        int destX = -1;
+        int destY = -1;
+        public GameState State = GameState.Select;//一开始默认是先选棋子
+        public currentPlayer Player = currentPlayer.red;//默认红方开始
+
+        public enum currentPlayer
+        {
+            black,
+            red
         }
+        public currentPlayer getPlayer()
+        {
+            return this.Player;
+        }
+        public void changePlayer()
+        {
+            if (Player == currentPlayer.red)
+                Player = currentPlayer.black;
+            else
+                Player = currentPlayer.red;
+        }
+        public enum GameState 
+        {
+            Select,
+            Move
+        }
+        public GameState getGameState()
+        {
+            return this.State;
+        }
+        public void setGameState(GameState state)
+        {
+            this.State = state;
+        }
+        public void changeGameState()
+        {
+            if (State == GameState.Move)
+                State = GameState.Select;
+            else
+                State = GameState.Move;
+        }
+
         //得到当前棋盘
-        public chesspiece[,] getBoard(){
+        public chesspiece[,] getBoard()
+        {
             return this.board;
         }
-        public void setBoard(chesspiece[,] board){
+        public void setBoard(chesspiece[,] board)
+        {
             this.board = board;
         }
-        public string getName(){
-            return this.Name;
+
+        public bool[,] ValidMove = new bool[10,9];
+        //List<Button> boardList = new List<Button>();
+        List<Button> buttons = new List<Button>();
+
+        public void StoredButton(Button button)
+        {
+           // List<Button> buttons = new List<Button>();
+           // boardList = buttons;
+            buttons.Add(button);
+            //for (int i = 0; i <buttons.Count; i++)
+            //{
+             // MessageBox.Show(" " + button.Name);
+            //}
+            // MessageBox.Show("添加了一个按钮");
+
         }
-        public int getRow(){
-            return this.rows;
-        }
-        public int getColumn(){
-            return this.columns;
+        public List<Button> getButtons()
+        {
+            //MessageBox.Show("" + buttons[0].Name);
+           // MessageBox.Show("" + buttons[1].Name);
+
+            return buttons;
         }
 
-        public void GiveThePiece()//给棋盘所有棋子赋值,初始化游戏棋盘
+        public GameBoard()//给棋盘所有棋子赋值,初始化游戏棋盘
         {
-            for ( int i = 0; i < board.GetLength(0); i++)//10
+            for (int i = 0; i < board.GetLength(0); i++)//10 
             {
-                for ( int j = 0; j < board.GetLength(1); j++)//9
+                for (int j = 0; j < board.GetLength(1); j++)//9 
                 {
-                    this.board[i,j] = new blank(chesspiece.Player_side.blank,i,j);//棋盘的(x，y)
+                     
+                    this.board[i, j] = new blank(chesspiece.Player_side.blank, i, j);//棋盘的(x，y)
                 }
             }
-            board[9, 0] = new chariot(chesspiece.Player_side.red,9,0);             //每个棋子的起始位置
-            board[9, 1] = new house(chesspiece.Player_side.red,9,1);
-            board[9, 2] = new elephant(chesspiece.Player_side.red,9,2);
-            board[9, 3] = new advisor(chesspiece.Player_side.red,9,3);
-            board[9, 4] = new general(chesspiece.Player_side.red,9,4);
-            board[9, 5] = new advisor(chesspiece.Player_side.red,9,5);
-            board[9, 6] = new elephant(chesspiece.Player_side.red,9,6);
-            board[9, 7] = new house(chesspiece.Player_side.red,9,7);
-            board[9, 8] = new chariot(chesspiece.Player_side.red,9,8);
-            board[7, 1] = new cannon(chesspiece.Player_side.red,7,1);
-            board[7, 7] = new cannon(chesspiece.Player_side.red,7,7);
-            board[6, 0] = new soldier(chesspiece.Player_side.red,6,0);
-            board[6, 2] = new soldier(chesspiece.Player_side.red,6,2);
-            board[6, 4] = new soldier(chesspiece.Player_side.red,6,4);
-            board[6, 6] = new soldier(chesspiece.Player_side.red,6,6);
-            board[6, 8] = new soldier(chesspiece.Player_side.red,6,8);
+            board[9, 0] = new chariot(chesspiece.Player_side.red, 9, 0);             //每个棋子的起始位置
+            board[9, 1] = new house(chesspiece.Player_side.red, 9, 1);
+            board[9, 2] = new elephant(chesspiece.Player_side.red, 9, 2);
+            board[9, 3] = new advisor(chesspiece.Player_side.red, 9, 3);
+            board[9, 4] = new general(chesspiece.Player_side.red, 9, 4);
+            board[9, 5] = new advisor(chesspiece.Player_side.red, 9, 5);
+            board[9, 6] = new elephant(chesspiece.Player_side.red, 9, 6);
+            board[9, 7] = new house(chesspiece.Player_side.red, 9, 7);
+            board[9, 8] = new chariot(chesspiece.Player_side.red, 9, 8);
+            board[7, 1] = new cannon(chesspiece.Player_side.red, 7, 1);
+            board[7, 7] = new cannon(chesspiece.Player_side.red, 7, 7);
+            board[6, 0] = new soldier(chesspiece.Player_side.red, 6, 0);
+            board[6, 2] = new soldier(chesspiece.Player_side.red, 6, 2);
+            board[6, 4] = new soldier(chesspiece.Player_side.red, 6, 4);
+            board[6, 6] = new soldier(chesspiece.Player_side.red, 6, 6);
+            board[6, 8] = new soldier(chesspiece.Player_side.red, 6, 8);
 
-            board[0, 0] = new chariot(chesspiece.Player_side.black,0,0);
-            board[0, 1] = new house(chesspiece.Player_side.black,0,1);
-            board[0, 2] = new elephant(chesspiece.Player_side.black,0,2);
-            board[0, 3] = new advisor(chesspiece.Player_side.black,0,3);
-            board[0, 4] = new general(chesspiece.Player_side.black,0,4);
-            board[0, 5] = new advisor(chesspiece.Player_side.black,0,5);
-            board[0, 6] = new elephant(chesspiece.Player_side.black,0,6);
-            board[0, 7] = new house(chesspiece.Player_side.black,0,7);
-            board[0, 8] = new chariot(chesspiece.Player_side.black,0,8);
-            board[2, 1] = new cannon(chesspiece.Player_side.black,2,1) ;
-            board[2, 7] = new cannon(chesspiece.Player_side.black,2,7);
-            board[3, 0] = new soldier(chesspiece.Player_side.black,3,0);
-            board[3, 2] = new soldier(chesspiece.Player_side.black,3,2);
-            board[3, 4] = new soldier(chesspiece.Player_side.black,3,4);
-            board[3, 6] = new soldier(chesspiece.Player_side.black,3,6);
-            board[3, 8] = new soldier(chesspiece.Player_side.black,3,8);
+            board[0, 0] = new chariot(chesspiece.Player_side.black, 0, 0);
+            board[0, 1] = new house(chesspiece.Player_side.black, 0, 1);
+            board[0, 2] = new elephant(chesspiece.Player_side.black, 0, 2);
+            board[0, 3] = new advisor(chesspiece.Player_side.black, 0, 3);
+            board[0, 4] = new general(chesspiece.Player_side.black, 0, 4);
+            board[0, 5] = new advisor(chesspiece.Player_side.black, 0, 5);
+            board[0, 6] = new elephant(chesspiece.Player_side.black, 0, 6);
+            board[0, 7] = new house(chesspiece.Player_side.black, 0, 7);
+            board[0, 8] = new chariot(chesspiece.Player_side.black, 0, 8);
+            board[2, 1] = new cannon(chesspiece.Player_side.black, 2, 1);
+            board[2, 7] = new cannon(chesspiece.Player_side.black, 2, 7);
+            board[3, 0] = new soldier(chesspiece.Player_side.black, 3, 0);
+            board[3, 2] = new soldier(chesspiece.Player_side.black, 3, 2);
+            board[3, 4] = new soldier(chesspiece.Player_side.black, 3, 4);
+            board[3, 6] = new soldier(chesspiece.Player_side.black, 3, 6);
+            board[3, 8] = new soldier(chesspiece.Player_side.black, 3, 8);
         }
-    
-        public void movePiece(int x ,int y,int x1, int y1){
-                chesspiece load  = new blank(chesspiece.Player_side.blank,x,y); 
-                //Console.WriteLine(load.getType());
-                
-                this.board[x,y].setX(x1);
-                this.board[x,y].setY(y1);
-                this.board[x1,y1] = this.board[x,y];
-                this.board[x,y] = load;
 
-                //Console.WriteLine(this.board[x,y].getType());
+        // 用来选择移动的棋子
+        public void Selectedpiece(Button button)
+        {
+            orginX = (int)button.GetValue(WindowNew.XQRowProperty);
+            orginY = (int)button.GetValue(WindowNew.XQColProperty);
+            //MessageBox.Show("这里是要移动的棋子！");
+        }
+        public int getOrginX()
+        {
+            return this.orginX;
+        }
+
+        public int getOrginY()
+        {
+            return this.orginY;
+        }
+
+
+        public void ChosenDest(Button button)
+        {
+            destX = (int)button.GetValue(WindowNew.XQRowProperty); 
+            destY = (int)button.GetValue(WindowNew.XQColProperty);
+            //MessageBox.Show("这里是目的地！");
+        }
+
+
+        public void movePiece()
+        {
+            chesspiece load = new blank(chesspiece.Player_side.blank, orginX, orginY);
+            //Console.WriteLine(load.getType());
+
+            //this.board[destX,destY] = this.board[orginX, orginY];
+            //this.board[destX, destY].setX(destX);
+            //this.board[destX, destY].setY(destY);
+            // this.board[orginX, orginY] = load;
+
+            this.board[orginX, orginY].setX(destX);
+            this.board[orginX, orginY].setY(destY);
+            this.board[destX, destY] = this.board[orginX, orginY];
+            this.board[orginX, orginY] = load;
+        }
+
+        public ArrayList getPieceCango()
+        {
+            ArrayList myList = new ArrayList();
+            myList = board[orginX,orginY].PlaceCanGo(this);
+            return myList;
         }
     }
 }
---
+++ WindowNew.xaml.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections;
+using System.Windows;
+using System.Drawing;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Media;
+using System.Collections.Generic;
+using System.Timers;
+using System.Threading;
+using System.Windows.Threading;
+using Timer = System.Timers.Timer;
+using System.Diagnostics;
+
+namespace XiangqiGame
+{
+    /// <summary>
+    /// Interaction logic for WindowNew.xaml
+    /// </summary>
+    ///  
+    public partial class WindowNew : Window
+    {
+        GameBoard board = new GameBoard(); //已经给所有棋子赋值，初始化成功
+        chesspiece[,] chesspiece = new chesspiece[10, 9];
+        display show = new display();
+        int min = 0, sec = 0;
+        DispatcherTimer timer = new DispatcherTimer();
+        public WindowNew()
+        {
+            InitializeComponent();
+            CreateGrid();
+            RedrawGrid();
+
+            //设置时间间隔与事件
+            timer.Interval = new TimeSpan(0, 0, 1);
+            timer.Tick += timer_Tick;
+            timer.IsEnabled = true;
+        }
+        //显示游戏进行时间
+        public void timer_Tick(object sender, EventArgs e)
+        {
+            sec++;
+            
+            if (sec > 60)
+            {
+                min++;
+                sec = 0;
+            }
+            Timer1.Text = "Game time: "+min + " : " + sec;
+        }
+
+        //依赖象棋的行列，此依赖属性用来储存Button的值，依赖宿主的类型是default(int)
+        public static readonly DependencyProperty XQRowProperty = DependencyProperty.Register("XQRow",
+                typeof(int),
+                typeof(Button),
+                new PropertyMetadata(default(int)));//定义棋盘行的属性 依赖属性就是可以自己没有值，并能够通过Binding从数据源获取值（依赖在别人身上）的属性
+        public static readonly DependencyProperty XQColProperty = DependencyProperty.Register("XQCol",
+                typeof(int),
+                typeof(Button),
+                new PropertyMetadata(default(int)));//定义棋盘列的属性
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //得到所点击button的行
+            int btnRow = (int)((Button)sender).GetValue(XQRowProperty);
+            int btnCol = (int)((Button)sender).GetValue(XQColProperty);
+            //MessageBox.Show("Button is: " + ((Button)sender).Name + "\n - Row = " + btnRow + "\n - Colum  = " + btnCol);
+
+            //HandleClick(btnCol, btnRow);
+            HandleButton((Button)sender);
+
+            //加按键声音
+            PlayClickSound();
+        }
+
+        public static SoundPlayer _sound = new SoundPlayer();
+
+        public static void PlayClickSound()
+        {
+            _sound.SoundLocation = "Sounds/click.wav";
+            _sound.Play();
+        }
+
+        Button storedButton = new Button();
+        int count = 0;
+        ArrayList list = new ArrayList();
+       
+        private void HandleButton(Button button)
+        {
+            chesspiece = board.getBoard();
+            if (board.getGameState() == GameBoard.GameState.Select)
+            {
+                int turn = count % 2;
+                bool round = show.currentRound(board, (int)button.GetValue(XQRowProperty), (int)button.GetValue(XQColProperty), turn);
+                if (round)
+                {
+                    board.changeGameState();
+                    board.Selectedpiece(button);
+                   // MessageBox.Show("" + button.Name);
+                    board.StoredButton(button);
+
+                    storedButton = button;
+                    list = MoveHint();
+                }
+                //选择棋子完后GameState => Move
+            }
+            else
+            {
+                // 在移动状态时要做到可以选择目的地
+                //MessageBox.Show("当前状态是Move");
+                board.ChosenDest(button);
+                board.changeGameState();
+                //MessageBox.Show("原坐标：" + (int)storedButton.GetValue(XQRowProperty) + "," + (int)storedButton.GetValue(XQColProperty) + "\n" + "目的地" + (int)button.GetValue(XQRowProperty) + "," + (int)button.GetValue(XQColProperty));
+                if (chesspiece[(int)storedButton.GetValue(XQRowProperty), (int)storedButton.GetValue(XQColProperty)].ValidMove(board, (int)storedButton.GetValue(XQRowProperty), (int)storedButton.GetValue(XQColProperty), (int)button.GetValue(XQRowProperty), (int)button.GetValue(XQColProperty)))
+                {
+                    count++;
+                    removeHint((int)button.GetValue(XQRowProperty), (int)button.GetValue(XQColProperty));
+                    board.changePlayer();
+                    board.movePiece();
+                   // MessageBox.Show("" + button.Name);
+                    board.StoredButton(button);
+                    //MessageBox.Show("" + stepcount);
+                    button.Background = storedButton.Background;
+                    storedButton.Background = null;
+                    show.isWine(board);
+                }
+                else
+                {
+                    MessageBox.Show("Please follow rules!");
+                    removeHint((int)button.GetValue(XQRowProperty), (int)button.GetValue(XQColProperty));
+                }
+                //GameState => Select
+            }
+            state.Text = "Current state: " + board.getGameState().ToString();
+            side.Text = "Current side: " + board.getPlayer().ToString();
+        }
+
+        public void CreateGrid() //创建一个网格对应棋盘
+        {
+            //此时已有一个初始化好了的chesspiece[,]棋盘名叫board在GameBoard中
+            //我要让名字为board1类型为chesspiece[,]的变量得到存储这初始化棋盘数据的二位数组board
+            chesspiece[,] board1 = new chesspiece[10, 9];
+            board1 = board.getBoard();
+
+            //display show = new display();
+            for (int i = 0; i < 10; i++)
+                GameBoardGrid.RowDefinitions.Add(new RowDefinition());
+            for (int i = 0; i < 9; i++)
+                //获取控件的ColumnDefinitions列表，并添加新建一个ColunmDefinition到最后
+                GameBoardGrid.ColumnDefinitions.Add(new ColumnDefinition());
+            for (int row = 0; row < 10; row++)
+            {
+                for (int col = 0; col < 9; col++)
+                {
+                    //遍历，在每个格上new一个button,名字叫做Button行列
+                    Button button = new Button();
+                    string name = "Button";
+                    name += row.ToString() + col.ToString();
+                    //必须要注册后面才能Find
+                    RegisterName(name, button);
+                    button.Name = "Button" + row.ToString() + col.ToString();
+                    //为每个button都添加一个路由事件
+                    button.Click += new RoutedEventHandler(this.Button_Click);
+                    //将row的值赋给XQRowProperty,设置按钮所在Grid控件的行
+                    button.SetValue(XQRowProperty, row);
+                    button.SetValue(XQColProperty, col);
+
+                    //设置附加属性row为给定的button
+                    Grid.SetRow(button, row);
+                    Grid.SetColumn(button, col);
+
+                    //将button添加到网格上
+                    GameBoardGrid.Children.Add(button);
+
+                    show.showChesspiece(row, col, button, board);
+                }
+            }
+        }
+
+        public void RedrawGrid()
+        {
+            ImageBrush bg = new ImageBrush();
+            bg.ImageSource = new BitmapImage(new Uri("Picture/ChineseChessGame/chessBoard.png", UriKind.RelativeOrAbsolute));
+            this.GameBoardGrid.Background = bg;
+        }
+
+        //移动时的高光提示
+        public ArrayList MoveHint()
+        {
+            ArrayList myList = new ArrayList();
+            myList = board.getPieceCango();
+            foreach (chesspiece piece in myList)
+            {
+                //MessageBox.Show("" + piece.getX() + piece.getY());
+                string name = "Button";
+                name += piece.getX();
+                name += piece.getY();
+
+                Button button = (Button)FindName(name);
+                // Button btn = (Button)this.Controls.Find("Name", false)[0];
+                //MessageBox.Show(""+name);
+                //MessageBox.Show("" + button.Name);
+
+                if (piece.getType() == XiangqiGame.chesspiece.Piece_type.blank)
+                {
+                    button.Opacity = 0.1;
+                    button.Background = new SolidColorBrush(Colors.Blue);
+                }
+            }
+            return myList;
+        }
+        //去掉高光提示
+        public void removeHint(int destX, int destY)
+        {
+            string orginName = "Button" + destX + destY;
+            ArrayList useList = new ArrayList();
+            useList = board.getPieceCango();
+            foreach (chesspiece piece in useList)
+            {
+                string name = "Button";
+                name += piece.getX();
+                name += piece.getY();
+
+                Button button = (Button)FindName(name);
+                if (button.Name.ToString() != orginName && piece.getType() == XiangqiGame.chesspiece.Piece_type.blank)
+                {
+                    button.Opacity = 0;
+                }
+                if (button.Name.ToString() == orginName)
+                {
+                    button.Opacity = 1;
+                }
+            }
+        }
+
+        //Exit
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBoxResult result = MessageBox.Show("Are you sure to quit the game?", "Exit", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+
+            {
+                Application.Current.MainWindow.Close();
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        List<Button> buttons = new List<Button>();
+        //悔棋功能
+        public void comeBackStep()
+        {
+            buttons = board.getButtons();
+            int num = -1;
+            foreach (Button button in buttons)
+            {
+                num++;
+            }
+            // MessageBox.Show("" + buttons[1].Name);
+            //起始位置
+            //Button lastbutton = buttons[buttons.Count];
+            //MessageBox.Show(lastbutton.Name.ToString());
+            int selectedX = (int)buttons[num].GetValue(XQRowProperty);
+            int selectedY = (int)buttons[num].GetValue(XQColProperty);
+            //MessageBox.Show("最后一个按钮位置为：" + selectedX + " " + selectedY);
+            //目的地
+            // Button destbutton = buttons[buttons.Count - 1];
+            int destX = (int)buttons[num - 1].GetValue(XQRowProperty);
+            int destY = (int)buttons[num - 1].GetValue(XQColProperty);
+            //MessageBox.Show("倒数第二个按钮位置为：" + destX + " " + destY);
+
+            chesspiece = board.getBoard();
+            chesspiece load = new blank(XiangqiGame.chesspiece.Player_side.blank, selectedX, selectedY);
+
+            chesspiece[selectedX, selectedY].setX(destX);
+            chesspiece[selectedX, selectedY].setY(destY);
+            chesspiece[destX, destY] = chesspiece[selectedX, selectedY];
+            chesspiece[selectedX, selectedY] = load;
+
+            //MessageBox.Show("最后一个按钮种类为：" + board.getBoard()[selectedX, selectedY].getType().ToString());
+            //MessageBox.Show("倒数第二个按钮种类为：" + board.getBoard()[destX, destY].getType().ToString());
+
+            buttons[num - 1].Background = buttons[num].Background;
+            buttons[num].Background = null;
+            //board.clean();
+            //MessageBox.Show("" + buttons.Count.ToString());
+            count--;
+            board.changePlayer();
+            state.Text = "Current state: " + board.getGameState().ToString();
+            side.Text = "Current side: " + board.getPlayer().ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("click2");
+            //List<Button> buttons = new List<Button>();
+            buttons = board.getButtons();
+            //MessageBox.Show(buttons.Count.ToString());
+           // if (buttons.Count >= 1 && buttons.Count % 2 != 0)
+           // {
+                comeBackStep();
+            //}
+        }
+    }
+}
